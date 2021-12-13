78cf64a89f270691d84754d2d4241835e0aebb59
Alexander Akulich <akulichalexander@gmail.com>
2021-12-13 20:10:30 +0300

    Client: Add infclass texture/skin

--- src/game/client/gameclient.h
+++ src/game/client/gameclient.h
@@ -548,6 +548,7 @@ public:
 	CNetObjHandler *GetNetObjHandler() override;
 
 	void LoadGameSkin(const char *pPath, bool AsDir = false);
+	void LoadInfclassSkin(const char *pPath, bool AsDir = false);
 	void LoadEmoticonsSkin(const char *pPath, bool AsDir = false);
 	void LoadParticlesSkin(const char *pPath, bool AsDir = false);
 	void LoadHudSkin(const char *pPath, bool AsDir = false);
@@ -643,6 +644,13 @@ public:
 
 	SClientGameSkin m_GameSkin;
 	bool m_GameSkinLoaded;
+
+	struct SClientInfclassSkin
+	{
+	};
+
+	SClientInfclassSkin m_InfclassSkin;
+	bool m_InfclassSkinLoaded;
 
 	struct SClientParticlesSkin
 	{
