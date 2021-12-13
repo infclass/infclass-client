f7c725e6119f13218bd0985793c433f0a6695796
Alexander Akulich <akulichalexander@gmail.com>
2021-12-13 20:10:30 +0300

    Client: Add infclass texture/skin

--- src/game/client/gameclient.h
+++ src/game/client/gameclient.h
@@ -528,6 +528,7 @@ public:
 	bool IsDisplayingWarning();
 
 	void LoadGameSkin(const char *pPath, bool AsDir = false);
+	void LoadInfclassSkin(const char *pPath, bool AsDir = false);
 	void LoadEmoticonsSkin(const char *pPath, bool AsDir = false);
 	void LoadParticlesSkin(const char *pPath, bool AsDir = false);
 
@@ -616,6 +617,13 @@ public:
 
 	SClientGameSkin m_GameSkin;
 	bool m_GameSkinLoaded;
+
+	struct SClientInfclassSkin
+	{
+	};
+
+	SClientInfclassSkin m_InfclassSkin;
+	bool m_InfclassSkinLoaded;
 
 	struct SClientParticlesSkin
 	{
