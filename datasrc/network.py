6b78271e3bfe5e517ace181ce9851c1717c5b2d2
Alexander Akulich <akulichalexander@gmail.com>
2021-12-15 22:49:55 +0300

    Protocol: Add InfClassPlayer network object

--- datasrc/network.py
+++ datasrc/network.py
@@ -34,6 +34,8 @@ ProjectileFlags = ["CLIENTID_BIT{}".format(i) for i in range(8)] + [
 
 Emoticons = ["OOP", "EXCLAMATION", "HEARTS", "DROP", "DOTDOT", "MUSIC", "SORRY", "GHOST", "SUSHI", "SPLATTEE", "DEVILTEE", "ZOMG", "ZZZ", "WTF", "EYES", "QUESTION"]
 
+InfClassPlayerFlags = ["INFECTED"]
+
 Powerups = ["HEALTH", "ARMOR", "WEAPON", "NINJA"]
 Authed = ["NO", "HELPER", "MOD", "ADMIN"]
 EntityClasses = ["PROJECTILE", "DOOR", "DRAGGER_WEAK", "DRAGGER_NORMAL", "DRAGGER_STRONG", "GUN_NORMAL", "GUN_EXPLOSIVE", "GUN_FREEZE", "GUN_UNFREEZE", "LIGHT", "PICKUP"]
@@ -90,6 +92,7 @@ Flags = [
 	Flags("GAMEINFOFLAG2", GameInfoFlags2),
 	Flags("EXPLAYERFLAG", ExPlayerFlags),
 	Flags("PROJECTILEFLAG", ProjectileFlags),
+	Flags("INFCLASS_PLAYER_FLAG", InfClassPlayerFlags),
 ]
 
 Objects = [
@@ -233,6 +236,11 @@ Objects = [
 
 	NetObjectEx("MyOwnObject", "my-own-object@heinrich5991.de", [
 		NetIntAny("m_Test"),
+	]),
+
+	NetObjectEx("InfClassPlayer", "player@infclass", [
+		NetIntAny("m_Flags"),
+		NetIntRange("m_Class", -1, 255),
 	]),
 
 	NetObjectEx("DDNetCharacter", "character@netobj.ddnet.tw", [
--- src/game/client/gameclient.cpp
+++ src/game/client/gameclient.cpp
@@ -1297,6 +1297,16 @@ void CGameClient::OnNewSnapshot()
 					}
 				}
 			}
+			else if(Item.m_Type == NETOBJTYPE_INFCLASSPLAYER)
+			{
+				const CNetObj_InfClassPlayer *pPlayerData = (const CNetObj_InfClassPlayer *)pData;
+
+				if(Item.m_ID < MAX_CLIENTS)
+				{
+					CClientData *pClient = &m_aClients[Item.m_ID];
+					ProcessInfClassPlayerInfo(pClient, pPlayerData);
+				}
+			}
 			else if(Item.m_Type == NETOBJTYPE_DDNETCHARACTER)
 			{
 				const CNetObj_DDNetCharacter *pCharacterData = (const CNetObj_DDNetCharacter *)pData;
@@ -2003,6 +2013,9 @@ void CGameClient::CClientData::Reset()
 	m_SkinInfo.m_ColorFeet = ColorRGBA(1, 1, 1);
 	m_SkinInfo.m_SkinMetrics.Reset();
 
+	m_InfClassPlayerFlags = 0;
+	m_InfClassPlayerClass = -1;
+
 	m_Solo = false;
 	m_Jetpack = false;
 	m_NoCollision = false;
@@ -2594,6 +2607,12 @@ vec2 CGameClient::GetSmoothPos(int ClientID)
 		}
 	}
 	return Pos;
+}
+
+void CGameClient::ProcessInfClassPlayerInfo(CClientData *pClient, const CNetObj_InfClassPlayer *pPlayerData)
+{
+	pClient->m_InfClassPlayerFlags = pPlayerData->m_Flags;
+	pClient->m_InfClassPlayerClass = pPlayerData->m_Class;
 }
 
 void CGameClient::Echo(const char *pString)
