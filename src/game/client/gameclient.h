8a0d00c2e4d88d999234c50142d9b87679dd1842
Alexander Akulich <akulichalexander@gmail.com>
2021-12-15 22:49:55 +0300

    Protocol: Add InfClassPlayer network object

--- src/game/client/gameclient.h
+++ src/game/client/gameclient.h
@@ -355,6 +355,9 @@ public:
 		int m_ColorBody;
 		int m_ColorFeet;
 
+		int m_InfClassPlayerFlags;
+		int m_InfClassPlayerClass;
+
 		char m_aName[MAX_NAME_LENGTH];
 		char m_aClan[MAX_CLAN_LENGTH];
 		int m_Country;
@@ -789,6 +792,7 @@ private:
 	void DetectStrongHook();
 
 	vec2 GetSmoothPos(int ClientId);
+	void ProcessInfClassPlayerInfo(int ClientId, const CNetObj_InfClassPlayer *pPlayerData);
 
 	int m_PredictedDummyId;
 	int m_IsDummySwapping;
