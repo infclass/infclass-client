d8a753590992cf57b59abb451da389e9282a6c5c
Alexander Akulich <akulichalexander@gmail.com>
2022-01-02 00:43:11 +0300

    Draft hook protection icon

--- src/game/client/gameclient.cpp
+++ src/game/client/gameclient.cpp
@@ -3071,6 +3071,8 @@ void CGameClient::LoadInfclassSkin(const char *pPath, bool AsDir)
 		Graphics()->UnloadTexture(&m_InfclassSkin.m_SpriteHookChain);
 		Graphics()->UnloadTexture(&m_InfclassSkin.m_SpriteHookHead);
 
+		Graphics()->UnloadTexture(&m_InfclassSkin.m_SpriteStatusHookable);
+
 		m_InfclassSkinLoaded = false;
 	}
 
@@ -3116,6 +3118,8 @@ void CGameClient::LoadInfclassSkin(const char *pPath, bool AsDir)
 
 		m_InfclassSkin.m_SpriteHookChain = Graphics()->LoadSpriteTexture(ImgInfo, &g_pData->m_aSprites[SPRITE_INF_HOOK_CHAIN]);
 		m_InfclassSkin.m_SpriteHookHead = Graphics()->LoadSpriteTexture(ImgInfo, &g_pData->m_aSprites[SPRITE_INF_HOOK_HEAD]);
+
+		m_InfclassSkin.m_SpriteStatusHookable = Graphics()->LoadSpriteTexture(ImgInfo, &g_pData->m_aSprites[SPRITE_INF_STATUS_HOOKABLE]);
 
 		m_InfclassSkinLoaded = true;
 
