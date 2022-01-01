e40eccf9f618af45a3ea62fbdf79f5c44e704119
Alexander Akulich <akulichalexander@gmail.com>
2022-01-02 02:44:21 +0300

    Add owner icon to own objects

--- src/game/client/prediction/gameworld.cpp
+++ src/game/client/prediction/gameworld.cpp
@@ -545,6 +545,10 @@ void CGameWorld::NetObjAdd(int ObjID, int ObjType, const void *pObjData, const C
 			}
 		}
 	}
+	else if(ObjType == NETOBJTYPE_INFCLASSOBJECT)
+	{
+
+	}
 }
 
 void CGameWorld::NetObjEnd()
