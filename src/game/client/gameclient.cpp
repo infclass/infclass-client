29cd34b458b4308e7249dce5f785dd4ddb11730e
Alexander Akulich <akulichalexander@gmail.com>
2022-01-02 00:43:11 +0300

    Draft hook protection icon

--- src/game/client/gameclient.cpp
+++ src/game/client/gameclient.cpp
@@ -3585,6 +3585,8 @@ void CGameClient::LoadInfclassSkin(const char *pPath, bool AsDir)
 		Graphics()->UnloadTexture(&m_InfclassSkin.m_SpriteHookChain);
 		Graphics()->UnloadTexture(&m_InfclassSkin.m_SpriteHookHead);
 
+		Graphics()->UnloadTexture(&m_InfclassSkin.m_SpriteStatusHookable);
+
 		m_InfclassSkinLoaded = false;
 	}
 
@@ -3630,6 +3632,8 @@ void CGameClient::LoadInfclassSkin(const char *pPath, bool AsDir)
 
 		m_InfclassSkin.m_SpriteHookChain = Graphics()->LoadSpriteTexture(ImgInfo, &g_pData->m_aSprites[SPRITE_INF_HOOK_CHAIN]);
 		m_InfclassSkin.m_SpriteHookHead = Graphics()->LoadSpriteTexture(ImgInfo, &g_pData->m_aSprites[SPRITE_INF_HOOK_HEAD]);
+
+		m_InfclassSkin.m_SpriteStatusHookable = Graphics()->LoadSpriteTexture(ImgInfo, &g_pData->m_aSprites[SPRITE_INF_STATUS_HOOKABLE]);
 
 		m_InfclassSkinLoaded = true;
 
--- src/game/client/gameclient.h
+++ src/game/client/gameclient.h
@@ -745,6 +745,8 @@ public:
 		// weapons and hook
 		IGraphics::CTextureHandle m_SpriteHookChain;
 		IGraphics::CTextureHandle m_SpriteHookHead;
+
+		IGraphics::CTextureHandle m_SpriteStatusHookable;
 	};
 
 	SClientInfclassSkin m_InfclassSkin;
