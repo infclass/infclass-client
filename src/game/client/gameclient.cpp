193b49d22d92c4135fe52a18dc83054bc3d1fb17
Alexander Akulich <akulichalexander@gmail.com>
2022-01-29 16:08:05 +0300

    Add bio mine icon

--- src/game/client/gameclient.cpp
+++ src/game/client/gameclient.cpp
@@ -3417,8 +3417,8 @@ IGraphics::CTextureHandle *CGameClient::GetInfclassTexturePtrForDamageType(DAMAG
 		return &m_InfclassSkin.m_SpriteSoldierBomb;
 	case DAMAGE_TYPE::SCIENTIST_MINE:
 		return &m_InfclassSkin.m_SpriteScientistMine;
-//	case DAMAGE_TYPE::BIOLOGIST_MINE:
-//		return &m_InfclassSkin.m_SpriteBiologistMine;
+	case DAMAGE_TYPE::BIOLOGIST_MINE:
+		return &m_InfclassSkin.m_SpriteBiologistMine;
 	case DAMAGE_TYPE::MERCENARY_BOMB:
 		return &m_InfclassSkin.m_SpriteMercenaryBomb;
 //	case DAMAGE_TYPE::WHITE_HOLE:
