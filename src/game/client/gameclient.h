25eb747e9dddc2703fae1a9863ac489b31b056d9
Alexander Akulich <akulichalexander@gmail.com>
2022-02-01 23:41:43 +0300

    Add custom infclass HUD for Hero class

--- src/game/client/gameclient.h
+++ src/game/client/gameclient.h
@@ -305,6 +305,10 @@ public:
 	int m_ServerMode;
 	CGameInfo m_GameInfo;
 
+	int m_TimeLimitInSeconds;
+	int m_InfClassHeroGiftTick;
+	int m_InfclassGameInfoVersion;
+
 	int m_DemoSpecId;
 
 	vec2 m_LocalCharacterPos;
@@ -384,6 +388,8 @@ public:
 		int m_InfClassPlayerFlags;
 		int m_InfClassPlayerClass;
 		bool m_InfClassCustomSkin;
+
+		int m_InfClassClassData1;
 
 		char m_aName[MAX_NAME_LENGTH];
 		char m_aClan[MAX_CLAN_LENGTH];
@@ -862,6 +868,8 @@ private:
 
 	vec2 GetSmoothPos(int ClientId);
 	void ProcessInfClassPlayerInfo(int ClientId, const CNetObj_InfClassPlayer *pPlayerData);
+	void ProcessInfClassClassInfo(int ClientId, const CNetObj_InfClassClassInfo *pClassInfo);
+	void ProcessInfClassGameInfo(const CNetObj_InfClassGameInfo *pGameInfo);
 
 	int m_PredictedDummyId;
 	int m_IsDummySwapping;
