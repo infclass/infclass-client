d4940be4ec35987d3d4b209627306dc4f027f151
Alexander Akulich <akulichalexander@gmail.com>
2022-02-06 22:10:27 +0300

    CPlayers::RenderPlayer: Boomer class has no weapon, so do not render it

--- src/engine/shared/infc_config_variables.h
+++ src/engine/shared/infc_config_variables.h
@@ -13,3 +13,4 @@
 MACRO_CONFIG_STR(ClAssetInfclass, cl_asset_infclass, 50, "default", CFGFLAG_SAVE | CFGFLAG_CLIENT, "The asset for infclass")
 MACRO_CONFIG_STR(ClInfcUpdatesUrl, cl_infc_updates_url, 256,  "https://infclass.github.io/files", CFGFLAG_SAVE | CFGFLAG_CLIENT, "The client updates URL")
 MACRO_CONFIG_INT(ClInfStatusSize, infc_status_size, 16, 4, 64, CFGFLAG_SAVE | CFGFLAG_CLIENT, "The size of infclass status icons")
+MACRO_CONFIG_INT(InfcShowBoomerWeapon, infc_show_boomer_weapon, 0, 0, 1, CFGFLAG_SAVE | CFGFLAG_CLIENT, "Toggle the Boomer hammer visibility")
