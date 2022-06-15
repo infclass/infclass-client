3bbc7cda51d58217df51e92cafc005aac4559c75
Alexander Akulich <akulichalexander@gmail.com>
2022-06-15 23:48:43 +0300

    Use custom ninja skin for infclass ninja class

--- src/game/client/gameclient.cpp
+++ src/game/client/gameclient.cpp
@@ -3188,6 +3188,9 @@ void CGameClient::ProcessInfClassPlayerInfo(int ClientId, const CNetObj_InfClass
 	const CSkin *pSkin = nullptr;
 	switch(pClient->m_InfClassPlayerClass)
 	{
+	case PLAYERCLASS_NINJA:
+		pSkin = m_Skins.Find("inf_ninja");
+		break;
 	default:
 		break;
 	}
