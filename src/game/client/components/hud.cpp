9d9a5f14eef70d4c81024aa112505b4ab7b4ac50
Alexander Akulich <akulichalexander@gmail.com>
2023-06-08 15:38:17 +0300

    Render the infclass extra HUD for spectators

--- src/game/client/components/hud.cpp
+++ src/game/client/components/hud.cpp
@@ -1958,6 +1958,10 @@ void CHud::OnRender()
 			{
 				RenderMovementInformation(SpectatorId);
 			}
+			if(SpectatorId != SPEC_FREEVIEW && GameClient()->m_GameInfo.m_InfClass)
+			{
+				RenderClassExtraHud(SpectatorId);
+			}
 			RenderSpectatorHud();
 		}
 
