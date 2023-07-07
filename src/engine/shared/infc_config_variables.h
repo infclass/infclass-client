7cdca7a861de929418e0afd21cfe6ed2807012fc
Alexander Akulich <akulichalexander@gmail.com>
2023-07-07 23:44:02 +0300

    Use DDRace HUD for Infclass (if available)

--- src/engine/shared/infc_config_variables.h
+++ src/engine/shared/infc_config_variables.h
@@ -14,4 +14,5 @@ MACRO_CONFIG_STR(ClAssetInfclass, cl_asset_infclass, 50, "default", CFGFLAG_SAVE
 MACRO_CONFIG_STR(ClInfcUpdatesUrl, cl_infc_updates_url, 256,  "https://infclass.github.io/files", CFGFLAG_SAVE | CFGFLAG_CLIENT, "The client updates URL")
 MACRO_CONFIG_INT(ClInfStatusSize, infc_status_size, 16, 4, 64, CFGFLAG_SAVE | CFGFLAG_CLIENT, "The size of infclass status icons")
 MACRO_CONFIG_INT(InfcShowBoomerWeapon, infc_show_boomer_weapon, 0, 0, 1, CFGFLAG_SAVE | CFGFLAG_CLIENT, "Toggle the Boomer hammer visibility")
+MACRO_CONFIG_INT(InfUseDDRaceHUD, infc_use_ddrace_hud, 1, 0, 1, CFGFLAG_SAVE | CFGFLAG_CLIENT, "Use DDRace (Health/Ammo/State) HUD for Infclass")
 MACRO_CONFIG_INT(InfcShowHookProtection, infc_show_hook_protection, 1, 0, 1, CFGFLAG_SAVE | CFGFLAG_CLIENT, "Show character hook protection status")
