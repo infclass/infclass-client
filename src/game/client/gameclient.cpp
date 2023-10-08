a08a21121c6039822457d34b158f6bceaeaf3b42
Alexander Akulich <akulichalexander@gmail.com>
2023-10-08 17:00:30 +0300

    Add a skin for Tank

--- src/game/client/gameclient.cpp
+++ src/game/client/gameclient.cpp
@@ -2960,6 +2960,9 @@ void CGameClient::ProcessInfClassPlayerInfo(int ClientId, const CNetObj_InfClass
 	case PLAYERCLASS_GHOST:
 		pSkin = m_Skins.Find("ghost");
 		break;
+	case PLAYERCLASS_TANK:
+		pSkin = m_Skins.Find("zombie");
+		break;
 	default:
 		break;
 	}
