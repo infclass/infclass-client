85a136cf44e6757049fbafdd3e88c03cec302e6a
Alexander Akulich <akulichalexander@gmail.com>
2023-10-29 02:57:09 +0300

    Support 'Invisible' infclass character property

--- src/game/client/gameclient.cpp
+++ src/game/client/gameclient.cpp
@@ -2472,6 +2472,7 @@ void CGameClient::CClientData::Reset()
 	m_InfClassPlayerClass = -1;
 	m_InfClassCustomSkin = false;
 
+	m_InfClassClassFlags = 0;
 	m_InfClassClassData1 = 0;
 
 	m_Solo = false;
@@ -3285,6 +3286,7 @@ void CGameClient::ProcessInfClassPlayerInfo(int ClientId, const CNetObj_InfClass
 void CGameClient::ProcessInfClassClassInfo(int ClientId, const CNetObj_InfClassClassInfo *pClassInfo)
 {
 	CClientData *pClient = &m_aClients[ClientId];
+	pClient->m_InfClassClassFlags = pClassInfo->m_Flags;
 	pClient->m_InfClassClassData1 = pClassInfo->m_Data1;
 }
 
