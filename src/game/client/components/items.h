afbab60494791885030b26712a3a64a030869220
Alexander Akulich <akulichalexander@gmail.com>
2023-11-04 23:11:16 +0300

    Implement client-side Scientist Mine rendering

--- src/game/client/components/items.h
+++ src/game/client/components/items.h
@@ -15,6 +15,7 @@ class CItems : public CComponent
 	void RenderFlag(const CNetObj_Flag *pPrev, const CNetObj_Flag *pCurrent, const CNetObj_GameData *pPrevGameData, const CNetObj_GameData *pCurGameData);
 	void RenderLaser(const CLaserData *pCurrent, bool IsPredicted = false);
 	void RenderInfclassObject(const struct CNetObj_InfClassObject *pCurrent, bool IsPredicted = false);
+	void RenderSciMine(const struct CNetObj_InfClassObject *pCurrent, bool IsPredicted);
 
 	int m_ItemsQuadContainerIndex;
 
