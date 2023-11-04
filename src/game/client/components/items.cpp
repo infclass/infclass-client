11ddbb8f7b0e26acdf6965c34a5dfc1aa9764f0a
Alexander Akulich <akulichalexander@gmail.com>
2023-11-04 23:11:16 +0300

    Implement client-side Scientist Mine rendering

--- src/game/client/components/items.cpp
+++ src/game/client/components/items.cpp
@@ -358,6 +358,18 @@ void CItems::RenderLaser(const CLaserData *pCurrent, bool IsPredicted)
 
 void CItems::RenderInfclassObject(const CNetObj_InfClassObject *pCurrent, bool IsPredicted)
 {
+	if(pCurrent->m_Flags & INFCLASS_OBJECT_FLAG_RELY_ON_CLIENTSIDE_RENDERING)
+	{
+		switch(pCurrent->m_Type)
+		{
+		case INFCLASS_OBJECT_TYPE_SCIENTIST_MINE:
+			RenderSciMine(pCurrent, IsPredicted);
+			break;
+		default:
+			break;
+		}
+	}
+
 	int LocalCID = m_pClient->m_aLocalIds[g_Config.m_ClDummy];
 	int SpecCID = m_pClient->m_Snap.m_SpecInfo.m_SpectatorId;
 	int CameraCID = m_pClient->m_Snap.m_SpecInfo.m_Active ? SpecCID : LocalCID;
@@ -403,6 +415,106 @@ void CItems::RenderInfclassObject(const CNetObj_InfClassObject *pCurrent, bool I
 		RenderTools()->RenderTee(CAnimState::GetIdle(), &RenderInfo, EMOTE_HAPPY, vec2(1, 0), IndicatorPos + Offset);
 	}
 #endif
+}
+
+void CItems::RenderSciMine(const CNetObj_InfClassObject *pCurrent, bool IsPredicted)
+{
+	float Radius = fx2f(pCurrent->m_ProximityRadius);
+	vec2 ObjPos(pCurrent->m_X, pCurrent->m_Y);
+
+	float Alpha = 1.f;
+	ColorRGBA RGB;
+
+	int ColorOut = g_Config.m_ClLaserRifleOutlineColor;
+	int ColorIn = g_Config.m_ClLaserRifleInnerColor;
+
+	RGB = color_cast<ColorRGBA>(ColorHSLA(ColorOut));
+	ColorRGBA OuterColor(RGB.r, RGB.g, RGB.b, 1.0f);
+	RGB = color_cast<ColorRGBA>(ColorHSLA(ColorIn));
+	ColorRGBA InnerColor(RGB.r, RGB.g, RGB.b, 1.0f);
+
+	constexpr int NumSide = 12;
+	constexpr float AngleStep = 2.0f * pi / NumSide;
+	vec2 aMinePoints[NumSide];
+	for(int i = 0; i < NumSide; i++)
+	{
+		aMinePoints[i] = ObjPos + direction(AngleStep * i) * Radius;
+	}
+
+	const float Len = distance(aMinePoints[0], aMinePoints[1]);
+
+	Graphics()->TextureClear();
+	Graphics()->QuadsBegin();
+	// do outline
+	Graphics()->SetColor(OuterColor.r, OuterColor.g, OuterColor.b, Alpha);
+	for(int i = 0; i < NumSide; i++)
+	{
+		bool Last = i == NumSide - 1;
+		const vec2 From = aMinePoints[i];
+		const vec2 Pos = Last ? aMinePoints[0] : aMinePoints[i + 1];
+
+		vec2 Dir = normalize_pre_length(Pos - From, Len);
+		vec2 Out = vec2(Dir.y, -Dir.x) * 6.0f;
+
+		IGraphics::CFreeformItem Freeform(
+			From.x - Out.x, From.y - Out.y,
+			From.x + Out.x, From.y + Out.y,
+			Pos.x - Out.x, Pos.y - Out.y,
+			Pos.x + Out.x, Pos.y + Out.y);
+		Graphics()->QuadsDrawFreeform(&Freeform, 1);
+	}
+
+	// do inner
+	Graphics()->SetColor(InnerColor.r, InnerColor.g, InnerColor.b, Alpha); // center
+	for(int i = 0; i < NumSide; i++)
+	{
+		bool Last = i == NumSide - 1;
+		const vec2 From = aMinePoints[i];
+		const vec2 Pos = Last ? aMinePoints[0] : aMinePoints[i + 1];
+
+		vec2 Dir = normalize_pre_length(Pos - From, Len);
+		vec2 Out;
+
+		Out = vec2(Dir.y, -Dir.x) * 4.0f;
+
+		IGraphics::CFreeformItem Freeform(
+			From.x - Out.x, From.y - Out.y,
+			From.x + Out.x, From.y + Out.y,
+			Pos.x - Out.x, Pos.y - Out.y,
+			Pos.x + Out.x, Pos.y + Out.y);
+		Graphics()->QuadsDrawFreeform(&Freeform, 1);
+	}
+	Graphics()->QuadsEnd();
+
+	float ObjectRenderTick = 0;
+	if(IsPredicted)
+		ObjectRenderTick = Client()->PredGameTick(g_Config.m_ClDummy) - pCurrent->m_StartTick + Client()->PredIntraGameTick(g_Config.m_ClDummy);
+	else
+		ObjectRenderTick = Client()->GameTick(g_Config.m_ClDummy) - pCurrent->m_StartTick + Client()->IntraGameTick(g_Config.m_ClDummy);
+
+	// render heads
+	// Use only the second particle (among 0, 1, 2)
+	const int CurParticle = 1;
+	Graphics()->TextureSet(GameClient()->m_ParticlesSkin.m_aSpriteParticleSplat[CurParticle]);
+	for(int i = 0; i < NumSide; i++)
+	{
+		const vec2 Pos = aMinePoints[i];
+		Graphics()->QuadsSetRotation((ObjectRenderTick + i * 1.5f) / pi);
+
+		Graphics()->SetColor(OuterColor.r, OuterColor.g, OuterColor.b, Alpha);
+		Graphics()->RenderQuadContainerAsSprite(m_ItemsQuadContainerIndex, m_aParticleSplatOffset[CurParticle], Pos.x, Pos.y);
+		Graphics()->SetColor(InnerColor.r, InnerColor.g, InnerColor.b, Alpha);
+		Graphics()->RenderQuadContainerAsSprite(m_ItemsQuadContainerIndex, m_aParticleSplatOffset[CurParticle], Pos.x, Pos.y, 20.f / 24.f, 20.f / 24.f);
+	}
+	Graphics()->QuadsSetRotation(0);
+
+	// Render hammer dots
+	for(int i = 0; i < 8; ++i)
+	{
+		float RandomRadius = random_float() * (Radius - 4.0f);
+		vec2 Pos = ObjPos + random_direction() * RandomRadius;
+		m_pClient->m_Effects.BulletTrail(Pos, Alpha);
+	}
 }
 
 void CItems::OnRender()
