7f66064857d74576d99650f27faab383c007f782
Alexander Akulich <akulichalexander@gmail.com>
2023-11-06 21:29:16 +0300

    Bump infclass version to v0.1.8 / 180

--- src/engine/shared/infclass.h
+++ src/engine/shared/infclass.h
@@ -1,7 +1,7 @@
 #ifndef ENGINE_CLIENT_INFCLASS_H
 #define ENGINE_CLIENT_INFCLASS_H
 
-#define INFCLASS_CLIENT_VERSION 175
+#define INFCLASS_CLIENT_VERSION 180
 
 int InfclassConfigDomainId();
 void SetInfclassConfigDomainId(int Id);
